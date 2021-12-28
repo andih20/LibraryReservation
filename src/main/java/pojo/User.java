c80a46d7e9278d0454d810e05f0db5afa78514d4
andih20 <2584038603@qq.com>
2021-12-28 14:55:06 +0800

    3commit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,40 @@
+package pojo;
+
+public class User {
+    private Integer uid;
+    private String uname;
+    private String upassword;
+    private String uemail;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpassword() {
+        return upassword;
+    }
+
+    public void setUpassword(String upassword) {
+        this.upassword = upassword;
+    }
+
+    public String getUemail() {
+        return uemail;
+    }
+
+    public void setUemail(String uemail) {
+        this.uemail = uemail;
+    }
+}
